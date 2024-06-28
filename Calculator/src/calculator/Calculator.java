a396bec7f7e96ea7a8a53d5e65e8d2e3b66c8df7
Adeyemo140 <91782691+Adeyemo140@users.noreply.github.com>
2024-06-28 10:56:35 +0100

    Add files via upload

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+import java.util.*;
+
+
+/**
+ *
+ * @author olade
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+          // Define your set of numbers
+    }
+    
+}
+
